adb898cab19c633dc69acd05393f6b391b2444a0
martacki <martha.frysztacki@gmail.com>
2025-11-10 17:46:02 +0100

    add solid biomass imports

--- scripts/walloon_scripts/BEWAL_potentials.py
+++ scripts/walloon_scripts/BEWAL_potentials.py
@@ -13,7 +13,8 @@ def update_BEWAL_potentials(n, planning_horizons, walloon_potentials=None):
 
     if walloon_potentials is not None:
         potentials=pd.read_csv(
-            walloon_potentials, index_col=0, dtype={'year': int}
+            walloon_potentials, index_col=0,
+            dtype={'year': int, 'value': float}
         ).query("year == @planning_horizons")
         print(potentials)
         
@@ -25,10 +26,24 @@ def update_BEWAL_potentials(n, planning_horizons, walloon_potentials=None):
                     n.generators[n.generators.bus.str.contains("BEWAL")]
                     .query("carrier == @carrier").index
                 )
-                print(BEWAL_carrier_idx)
-                print(n.generators.loc[BEWAL_carrier_idx, "p_nom_max"])
                 n.generators.loc[BEWAL_carrier_idx, "p_nom_max"] = potentials.loc[carrier].value
-                print(n.generators.loc[BEWAL_carrier_idx, "p_nom_max"])
+            elif carrier == 'solid biomass import':
+                # remove all solid biomass imports except the one for BEWAL
+                # and set the import potential to the one given for BEWAL
+                biomass_imports = n.stores.query("carrier == @carrier")
+
+                if "GW" in potentials.loc[carrier].unit:
+                    e_nom = potentials.loc[carrier].value * 1000
+                else:
+                    e_nom = potentials.loc[carrier].value
+                for col in ["e_nom_min", "e_nom", "e_nom_max", "e_initial"]:
+                    n.stores.loc[biomass_imports.index, col] = e_nom
+
+                print(n.stores.loc[biomass_imports.index].iloc[0])
+                biomass_imports = biomass_imports.bus.values
+                biomass_imports = n.links.query("bus0 in @biomass_imports").index
+                drop_non_BEWAL_imports = [link for link in biomass_imports if "BEWAL" not in link]
+                n.remove("Link", drop_non_BEWAL_imports)
             else:
                 logger.warning(
                     f"{carrier} is currently not a supported or valid technology."
