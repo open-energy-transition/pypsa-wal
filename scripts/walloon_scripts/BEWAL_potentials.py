6fa07ffaf1937beaa9c727d0e36dd6c7ed4bbd8d
Aisling Pigott <ash.pigott@gmail.com>
2025-11-10 21:10:00 -0700

    syntax fix for biogas and biomass initial implementation

--- scripts/walloon_scripts/BEWAL_potentials.py
+++ scripts/walloon_scripts/BEWAL_potentials.py
@@ -24,23 +24,38 @@ def update_BEWAL_potentials(n, planning_horizons, walloon_potentials=None):
                     .query("carrier == @carrier").index
                 )
                 n.generators.loc[BEWAL_carrier_idx, "p_nom_max"] = potentials.loc[carrier].value
-            for biocarrier in ['solid biomass import', 'biogas']
-                elif carrier.contains(biocarrier):
-                    # remove all solid biomass imports except the one for BEWAL
-                    # and set the import potential to the one given for BEWAL
-                    biomass_imports = n.stores.query("carrier == @carrier")
+            elif 'solid biomass import (oustide europe)' in carrier:
+                # remove all solid biomass imports except the one for BEWAL
+                # and set the import potential to the one given for BEWAL
+                biomass_imports = n.stores.query("carrier == @carrier")
 
-                    if "GW" in potentials.loc[carrier].unit:
-                        e_nom = potentials.loc[carrier].value * 1000
-                    else:
-                        e_nom = potentials.loc[carrier].value
-                    for col in ["e_nom_min", "e_nom", "e_nom_max", "e_initial"]:
-                        n.stores.loc[biomass_imports.index, col] = e_nom
+                if "GW" in potentials.loc[carrier].unit:
+                    e_nom = potentials.loc[carrier].value * 1000
+                else:
+                    e_nom = potentials.loc[carrier].value
+                for col in ["e_nom_min", "e_nom", "e_nom_max", "e_initial"]:
+                    n.stores.loc[biomass_imports.index, col] = e_nom
 
-                    biomass_imports = biomass_imports.bus.values
-                    biomass_imports = n.links.query("bus0 in @biomass_imports").index
-                    drop_non_BEWAL_imports = [link for link in biomass_imports if "BEWAL" not in link]
-                    n.remove("Link", drop_non_BEWAL_imports)po
+                biomass_imports = biomass_imports.bus.values
+                biomass_imports = n.links.query("bus0 in @biomass_imports").index
+                drop_non_BEWAL_imports = [link for link in biomass_imports if "BEWAL" not in link]
+                n.remove("Link", drop_non_BEWAL_imports)
+            elif 'biogas' in carrier:
+                # remove all solid biomass imports except the one for BEWAL
+                # and set the import potential to the one given for BEWAL
+                biomass_imports = n.stores.query("carrier == @carrier")
+
+                if "GW" in potentials.loc[carrier].unit:
+                    e_nom = potentials.loc[carrier].value * 1000
+                else:
+                    e_nom = potentials.loc[carrier].value
+                for col in ["e_nom_min", "e_nom", "e_nom_max", "e_initial"]:
+                    n.stores.loc[biomass_imports.index, col] = e_nom
+
+                biomass_imports = biomass_imports.bus.values
+                biomass_imports = n.links.query("bus0 in @biomass_imports").index
+                drop_non_BEWAL_imports = [link for link in biomass_imports if "BEWAL" not in link]
+                n.remove("Link", drop_non_BEWAL_imports)
             else:
                 logger.warning(
                     f"{carrier} is currently not a supported or valid technology."
