29ea071e5969495f2cf73de8281ede1056408923
Aisling Pigott <ash.pigott@gmail.com>
2025-11-10 20:42:53 -0700

    update to have consistent naming

--- scripts/walloon_scripts/BEWAL_potentials.py
+++ scripts/walloon_scripts/BEWAL_potentials.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 
 
 def update_BEWAL_potentials(n, planning_horizons, walloon_potentials=None):
-
     if walloon_potentials is not None:
         potentials=pd.read_csv(
             walloon_potentials, index_col=0,
@@ -25,22 +24,23 @@ def update_BEWAL_potentials(n, planning_horizons, walloon_potentials=None):
                     .query("carrier == @carrier").index
                 )
                 n.generators.loc[BEWAL_carrier_idx, "p_nom_max"] = potentials.loc[carrier].value
-            elif carrier == 'solid biomass import':
-                # remove all solid biomass imports except the one for BEWAL
-                # and set the import potential to the one given for BEWAL
-                biomass_imports = n.stores.query("carrier == @carrier")
+            for biocarrier in ['solid biomass import', 'biogas']
+                elif carrier.contains(biocarrier):
+                    # remove all solid biomass imports except the one for BEWAL
+                    # and set the import potential to the one given for BEWAL
+                    biomass_imports = n.stores.query("carrier == @carrier")
 
-                if "GW" in potentials.loc[carrier].unit:
-                    e_nom = potentials.loc[carrier].value * 1000
-                else:
-                    e_nom = potentials.loc[carrier].value
-                for col in ["e_nom_min", "e_nom", "e_nom_max", "e_initial"]:
-                    n.stores.loc[biomass_imports.index, col] = e_nom
+                    if "GW" in potentials.loc[carrier].unit:
+                        e_nom = potentials.loc[carrier].value * 1000
+                    else:
+                        e_nom = potentials.loc[carrier].value
+                    for col in ["e_nom_min", "e_nom", "e_nom_max", "e_initial"]:
+                        n.stores.loc[biomass_imports.index, col] = e_nom
 
-                biomass_imports = biomass_imports.bus.values
-                biomass_imports = n.links.query("bus0 in @biomass_imports").index
-                drop_non_BEWAL_imports = [link for link in biomass_imports if "BEWAL" not in link]
-                n.remove("Link", drop_non_BEWAL_imports)
+                    biomass_imports = biomass_imports.bus.values
+                    biomass_imports = n.links.query("bus0 in @biomass_imports").index
+                    drop_non_BEWAL_imports = [link for link in biomass_imports if "BEWAL" not in link]
+                    n.remove("Link", drop_non_BEWAL_imports)po
             else:
                 logger.warning(
                     f"{carrier} is currently not a supported or valid technology."
