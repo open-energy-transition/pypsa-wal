47ea0f3a2b7e93bba0297da426698d2ee158c473
Measrainsey Meng <measrainsey@pm.me>
2025-11-18 06:43:14 +0100

    chore: revert hotfix: implement p_nom_min on nuclear links

--- scripts/add_existing_baseyear.py
+++ scripts/add_existing_baseyear.py
@@ -818,11 +818,6 @@ if __name__ == "__main__":
     sanitize_custom_columns(n)
     sanitize_carriers(n, snakemake.config)
 
-    agg_limits_file = (
-        snakemake.config.get("solving", {})
-        .get("agg_p_nom_limits", {})
-        .get("file")
-    )
     add_BEWAL_nuclear(
         n=n,
         planning_horizon=int(snakemake.wildcards.planning_horizons),
@@ -830,7 +825,6 @@ if __name__ == "__main__":
             snakemake.config["electricity"]["extendable_carriers"]
             .get("extendable_nuclear_links", {})
         ),
-        agg_limit_file=agg_limits_file,
     )
 
     n.export_to_netcdf(snakemake.output[0])
