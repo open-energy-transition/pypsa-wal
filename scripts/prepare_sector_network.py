e6829901b191cc430449b4381571c265c6055816
martacki <martha.frysztacki@gmail.com>
2025-11-20 22:38:09 +0100

    push CCL expansion to custom BEWAL potentials

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -22,8 +22,6 @@ from networkx.algorithms.connectivity.edge_augmentation import k_edge_augmentati
 from pypsa.geo import haversine_pts
 from scipy.stats import beta
 
-from scripts.walloon_scripts.BEWAL_potentials import update_BEWAL_potentials
-
 from scripts._helpers import (
     configure_logging,
     get,
@@ -6558,11 +6556,5 @@ if __name__ == "__main__":
 
     sanitize_carriers(n, snakemake.config)
     sanitize_locations(n)
-
-    update_BEWAL_potentials(
-        n=n,
-        planning_horizons=int(snakemake.wildcards.planning_horizons),
-        walloon_potentials=snakemake.config["electricity"].get("walloon_potentials", None),
-        )
 
     n.export_to_netcdf(snakemake.output[0])
